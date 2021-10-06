621f17f6594a93d6b79c622450c9670370db6b2f
Sara Stankiewicz <sarastankiewicz@macbook-air.partners.org>
2021-10-06 16:29:52 -0400

    removing the ontoProcR.proj file"

--- R/ontoProcData-package.R
+++ R/ontoProcData-package.R
@@ -1,9 +1,9 @@
-#' Title
-#' 
-#' @description
+#' A data package for ontoProc
+#'
+#' @description This package manages rda files of multiple ontologies that are used in the ontoProc package. These ontologies were originally downloaded as owl or obo files and converted into Rda files . The files were downloaded at various times but most of them were downloaded on June 22 2021.
 #'
 #' @name ontoProcData-package
 #' @aliases ontoProcData-package ontoProcData
 #' @docType package
-#' @author
+#' @author Sara Stankiewicz
 NULL
